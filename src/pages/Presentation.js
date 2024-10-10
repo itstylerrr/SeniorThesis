823181222c43bdd234d9fe039440bdd61b244e15
tyler <63240641+itstylerrr@users.noreply.github.com>
2024-10-09 19:30:48 -0500

    new updates

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -5,43 +5,50 @@ import img2 from '../assets/photos/22424_DistrictChamps-69.jpg';
 import img3 from '../assets/photos/82024_PvJ-15.jpg';
 import annotation from '../components/Annotations.json';
 
+// Image array with annotations
 const images = [
-    { src: img1, alt: annotation.images.img1.name, annotation: annotation.images.img1.annotation },
-  { src: img2, alt: annotation.images.img2.name, annotation: annotation.images.img2.annotation },
-  { src: img3, alt: annotation.images.img3.name, annotation: annotation.images.img3.annotation },
-  // Add more images as needed
+  { src: img1, alt: annotation.images.img1.name, annotation: annotation.images.img1.annotation, link: annotation.images.img1.link },
+  { src: img2, alt: annotation.images.img2.name, annotation: annotation.images.img2.annotation, link: annotation.images.img2.link },
+  { src: img3, alt: annotation.images.img3.name, annotation: annotation.images.img3.annotation, link: annotation.images.img3.link },
 ];
 
 const Presentation = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [fadeClass, setFadeClass] = useState('fade-in');
-  
-    // Automatically switch images every 7 seconds
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setFadeClass(''); // Remove fade class before switching
-        setTimeout(() => {
-          setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-          setFadeClass('fade-in'); // Add fade class after switching
-        }, 100); // Delay to reset the fade
-      }, 10000);
-  
-      return () => clearInterval(interval);
-    }, []);
-  
-    const currentImage = images[currentIndex];
-  
-    return (
-      <div className="presentation-container">
-        <div className="image-container">
-          <img src={currentImage.src} alt={`Slide ${currentIndex + 1}`} className={`presentation-image ${fadeClass}`} />
-        </div>
-        <div className="annotation-container">
-          <h2>{currentImage.alt}</h2>
-          <p>{currentImage.annotation}</p>
-        </div>
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fadeClass, setFadeClass] = useState('fade-in');
+
+  // Automatically switch images every 7 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFadeClass(''); // Remove fade class before switching
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setFadeClass('fade-in'); // Add fade class after switching
+      }, 100); // Delay to reset the fade
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const currentImage = images[currentIndex];
+
+  return (
+    <div className="presentation-container">
+      <div className='overlay' />
+      {/* Image container with auto-scaling */}
+      <div className="image-container">
+        <img src={currentImage.src} alt={`Slide ${currentIndex + 1}`} className={`presentation-image ${fadeClass}`} />
       </div>
-    );
-  };
-  
-  export default Presentation;
+
+      {/* Annotations for each image */}
+      <div className="annotation-container">
+        <h2>{currentImage.alt}</h2>
+        <p>{currentImage.annotation}</p>
+        <a href={currentImage.link} title='Visit the album!' target='_blank' rel="noreferrer noopener">
+          Click here to visit the album!
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Presentation;
