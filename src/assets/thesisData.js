c4c69d1186f8411bff30a2f039c953ff7a947cc2
tyler <63240641+itstylerrr@users.noreply.github.com>
2024-12-02 11:13:58 -0600

    add annotations

--- src/assets/thesisData.js
+++ src/assets/thesisData.js
@@ -8,11 +8,12 @@ const thesisSections = [
       "/images/determination-3.jpg",
     ],
     galleryAnnotations: [
-      "Annotation for Image 1",
+      "In a Priory Varsity Hockey game against Ladue on November 22nd, 2024, Junior Freddy Gaertner handles the puck through the Ladue defense. That is until he was tripped by a Ladue player who is out of frame. Even after being tripped, seemingly floating in the air, still has his eyes focused on moving forward. To capture this shot, I made sure my camera was shooting at a high shutter speed to freeze the action, specifically, it was at 1/800s!",
       "Annotation for Image 2",
       "Annotation for Image 3",
     ],
-    annotation: "Annotation for Determination will go here.",
+    annotation:
+      "To me, a great shot that embodies determination in sports photography requires a diverse set of skills. Capturing an athlete's pure grit and perseverance involves getting up close to the action while remaining off the field, rink, or court. For this, I rely on my Sigma 60-600mm lens, which offers outstanding versatility in sports photography. This lens allows me to capture incredibly detailed close-up shots, showcasing the intensity and passion in an athlete's expression, while also giving me the ability to zoom out sufficiently to encompass the broader scene and all of the unfolding action. \n In this section, you will find a selection of some of my favorite photographs featuring Priory athletes, each highlighting their unwavering determination to win. These images encapsulate the moments that truly define dedication and hard work in sports. To dive deeper into the story behind each smaller image, simply click on the image itself for annotations and insights on what makes each shot unique and meaningful. I hope you enjoy viewing these moments as much as I enjoyed capturing them!",
   },
   {
     title: "excitement",
